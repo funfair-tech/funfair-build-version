406c847c496dd8f71ba03b2ff84adee3b5e3715a
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-02-10 14:59:51 +0000

    Added tests for simple branch classification

---
+++ src/FunFair.BuildVersion.Detection.Tests/BranchClassificationTests.cs
@@ -0,0 +1,28 @@
+﻿using FunFair.BuildVersion.Interfaces;
+using FunFair.Test.Common;
+using NuGet.Versioning;
+using Xunit;
+
+namespace FunFair.BuildVersion.Detection.Tests
+{
+    public sealed class SimpleBranchClassificationTests : TestBase
+    {
+        private readonly IBranchClassification _branchClassifcation;
+
+        public SimpleBranchClassificationTests()
+        {
+            this._branchClassifcation = new BranchClassification();
+        }
+
+        [Theory]
+        [InlineData("master")]
+        [InlineData("feature/update-branch")]
+        [InlineData("depends/update-components")]
+        public void ShouldNotBeConsideredRelease(string branchName)
+        {
+            bool isRelease = this._branchClassifcation.IsReleaseBranch(branchName: branchName, out NuGetVersion? version);
+            Assert.False(condition: isRelease, userMessage: "Branch should not be considered a release branch");
+            Assert.Null(version);
+        }
+    }
+}
