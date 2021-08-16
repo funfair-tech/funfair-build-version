fdeda26d5074c807d5c943068e69869251f60ba6
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-08-16 22:29:32 +0100

    Made version more resilient to being built as a single exe

--- src/FunFair.BuildVersion/ExecutableVersionInformation.cs
+++ src/FunFair.BuildVersion/ExecutableVersionInformation.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace FunFair.BuildVersion
 {
@@ -12,9 +17,52 @@ namespace FunFair.BuildVersion
 
         private static string CommonVersion(Type type)
         {
-            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(type.Assembly.Location);
+            Assembly assembly = type.Assembly;
+
+            return GetAssemblyFileVersionFile() ?? GetAssemblyFileVersion(assembly) ?? GetAssemblyVersion(assembly);
+        }
+
+        private static string? GetAssemblyFileVersionFile()
+        {
+            IReadOnlyList<string> args = Environment.GetCommandLineArgs();
+
+            if (args.Count == 0)
+            {
+                return null;
+            }
+
+            string location = args[0];
+
+            if (string.IsNullOrWhiteSpace(location) || !File.Exists(location))
+            {
+                return null;
+            }
+
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
 
             return fileVersionInfo.ProductVersion!;
+        }
+
+        private static string? GetAssemblyFileVersion(Assembly assembly)
+        {
+            AssemblyFileVersionAttribute? fvi = assembly.GetCustomAttributes<AssemblyFileVersionAttribute>()
+                                                        .FirstOrDefault();
+
+            return fvi?.Version;
+        }
+
+        private static string GetAssemblyVersion(Assembly assembly)
+        {
+            Console.Write("Finding Assembly Version");
+            Version? assemblyVersion = assembly.GetName()
+                                               .Version;
+
+            if (assemblyVersion == null)
+            {
+                throw new VersionNotFoundException();
+            }
+
+            return assemblyVersion.ToString();
         }
     }
 }
