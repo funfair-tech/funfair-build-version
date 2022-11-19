77e283d58f5aad2cdde7d0c2926dbc1d18612893
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-19 23:47:43 +0000

    [FF-2244] - Code cleanup on BuildVersion.sln

--- src/FunFair.BuildVersion/Services/LoggerProxy.cs
+++ src/FunFair.BuildVersion/Services/LoggerProxy.cs
@@ -16,7 +16,8 @@ public sealed class LoggerProxy<TLogClass> : ILogger<TLogClass>
     ///     Constructor.
     /// </summary>
     /// <param name="logger">Logger.</param>
-    public LoggerProxy([SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0024: Logger parameters should be ILogger<T>", Justification = "Not created through DI")] ILogger logger)
+    public LoggerProxy(
+        [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0024: Logger parameters should be ILogger<T>", Justification = "Not created through DI")] ILogger logger)
     {
         this._diagnosticLogger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
