3738ed325a999c516c5fa976ce3273476f65d4d4
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 08:36:19 +0000

    Cleanup: GitBranchDiscoveryLoggingExtensions.cs

--- src/FunFair.BuildVersion.Detection/LoggingExtensions/GitBranchDiscoveryLoggingExtensions.cs
+++ src/FunFair.BuildVersion.Detection/LoggingExtensions/GitBranchDiscoveryLoggingExtensions.cs
@@ -7,15 +7,8 @@ internal static partial class GitBranchDiscoveryLoggingExtensions
     [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Head SHA: {sha}")]
     public static partial void LogHeadSha(this ILogger<GitBranchDiscovery> logger, string sha);
 
-    [LoggerMessage(
-        EventId = 2,
-        Level = LogLevel.Information,
-        Message = "Pull Request: {pullRequestId}"
-    )]
-    public static partial void LogPullRequest(
-        this ILogger<GitBranchDiscovery> logger,
-        long pullRequestId
-    );
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "Pull Request: {pullRequestId}")]
+    public static partial void LogPullRequest(this ILogger<GitBranchDiscovery> logger, long pullRequestId);
 
     [LoggerMessage(
         EventId = 3,
