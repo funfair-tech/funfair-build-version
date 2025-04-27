f9429b38aba6a5e6d66611bbdda89313ab9940c8
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 08:45:12 +0000

    Cleanup: GitHubContext.cs

--- src/FunFair.BuildVersion.GitTagBuildNumber/Github/GitHubContext.cs
+++ src/FunFair.BuildVersion.GitTagBuildNumber/Github/GitHubContext.cs
@@ -3,9 +3,4 @@ using System.Diagnostics;
 namespace FunFair.BuildVersion.GitTagBuildNumber.Github;
 
 [DebuggerDisplay("Repository: {Repository} Sha: {Sha}")]
-public readonly record struct GitHubContext(
-    string Token,
-    string Repository,
-    string Sha,
-    string Prefix
-);
+public readonly record struct GitHubContext(string Token, string Repository, string Sha, string Prefix);
