b35fd0eef6da6753560a2fda0c90d20b9243ad5c
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-06-02 18:02:24 +0100

    More logging

--- src/FunFair.BuildVersion.GitTagBuildNumber/Github/BuildTagNumber.cs
+++ src/FunFair.BuildVersion.GitTagBuildNumber/Github/BuildTagNumber.cs
@@ -21,42 +21,23 @@ public static class BuildTagNumber
 {
     private const int MAX_ATTEMPTS = 10;
 
-    public static async ValueTask<int> GetNextBuildNumberAsync(
-        GitHubContext context,
-        CancellationToken cancellationToken
-    )
+    private const string MIME_TYPE = "application/vnd.github+json";
+
+    public static async ValueTask<int> GetNextBuildNumberAsync(GitHubContext context, CancellationToken cancellationToken)
     {
         using (HttpClient httpClient = CreateClient(context))
         {
-            int currentVersion = await GetCurrentAsync(
-                context: context,
-                httpClient: httpClient,
-                cancellationToken: cancellationToken
-            );
+            int currentVersion = await GetCurrentAsync(context: context, httpClient: httpClient, cancellationToken: cancellationToken);
 
-            return await SetCurrentVersionAsync(
-                context: context,
-                nextBuildNumber: ++currentVersion,
-                httpClient: httpClient,
-                cancellationToken: cancellationToken
-            );
+            return await SetCurrentVersionAsync(context: context, nextBuildNumber: ++currentVersion, httpClient: httpClient, cancellationToken: cancellationToken);
         }
     }
 
-    private static async ValueTask<int> GetCurrentAsync(
-        GitHubContext context,
-        HttpClient httpClient,
-        CancellationToken cancellationToken
-    )
+    private static async ValueTask<int> GetCurrentAsync(GitHubContext context, HttpClient httpClient, CancellationToken cancellationToken)
     {
         Uri uri = new($"https://api.github.com/repos/{context.Repository}/git/refs/tags/{context.Prefix}build-number-");
 
-        using (
-            HttpResponseMessage result = await httpClient.GetAsync(
-                requestUri: uri,
-                cancellationToken: cancellationToken
-            )
-        )
+        using (HttpResponseMessage result = await httpClient.GetAsync(requestUri: uri, cancellationToken: cancellationToken))
         {
             if (result.StatusCode == HttpStatusCode.NotFound)
             {
@@ -75,10 +56,11 @@ public static class BuildTagNumber
                 IReadOnlyList<GithubTagReference>? items = await JsonSerializer.DeserializeAsync(
                     utf8Json: stream,
                     jsonTypeInfo: GithubApiJsonSerializerContext.Default.IReadOnlyListGithubTagReference,
-                    cancellationToken: cancellationToken
-                );
+                    cancellationToken: cancellationToken);
 
-                return items is null ? 0 : ExtractVersionFromTags(items);
+                return items is null
+                    ? 0
+                    : ExtractVersionFromTags(items);
             }
         }
     }
@@ -96,28 +78,16 @@ public static class BuildTagNumber
             return 0;
         }
 
-        return items
-            .Select(item => GitUrlProtocolRegex.BuildNumbersFromTag().Match(item.Reference))
-            .Where(match => match.Success)
-            .Select(match => match.Groups["BuildNumber"].Value)
-            .Max(versionText =>
-                int.TryParse(
-                    s: versionText,
-                    style: NumberStyles.Integer,
-                    provider: CultureInfo.InvariantCulture,
-                    out int version
-                )
-                    ? version
-                    : 0
-            );
+        return items.Select(item => GitUrlProtocolRegex.BuildNumbersFromTag()
+                                                       .Match(item.Reference))
+                    .Where(match => match.Success)
+                    .Select(match => match.Groups["BuildNumber"].Value)
+                    .Max(versionText => int.TryParse(s: versionText, style: NumberStyles.Integer, provider: CultureInfo.InvariantCulture, out int version)
+                             ? version
+                             : 0);
     }
 
-    private static async ValueTask<int> SetCurrentVersionAsync(
-        GitHubContext context,
-        int nextBuildNumber,
-        HttpClient httpClient,
-        CancellationToken cancellationToken
-    )
+    private static async ValueTask<int> SetCurrentVersionAsync(GitHubContext context, int nextBuildNumber, HttpClient httpClient, CancellationToken cancellationToken)
     {
         Uri uri = new($"https://api.github.com/repos/{context.Repository}/git/refs");
 
@@ -127,13 +97,7 @@ public static class BuildTagNumber
         {
             using (StringContent stringContent = BuildAddTagModel(context: context, nextBuildNumber: nextBuildNumber))
             {
-                using (
-                    HttpResponseMessage result = await httpClient.PostAsync(
-                        requestUri: uri,
-                        content: stringContent,
-                        cancellationToken: cancellationToken
-                    )
-                )
+                using (HttpResponseMessage result = await httpClient.PostAsync(requestUri: uri, content: stringContent, cancellationToken: cancellationToken))
                 {
                     if (result.StatusCode == HttpStatusCode.Created)
                     {
@@ -142,17 +106,16 @@ public static class BuildTagNumber
 
                     if (result.StatusCode == HttpStatusCode.UnprocessableContent)
                     {
+                        Console.WriteLine($"Retrying updating to build number: {nextBuildNumber} (Attempt {attempts} of {MAX_ATTEMPTS})");
                         ++attempts;
                         await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
                         continue;
                     }
 
                     if (result.StatusCode != HttpStatusCode.Conflict)
                     {
-                        return CouldNotUpdateBuildNumberHttpError(
-                            nextBuildNumber: nextBuildNumber,
-                            statusCode: result.StatusCode
-                        );
+                        return CouldNotUpdateBuildNumberHttpError(nextBuildNumber: nextBuildNumber, statusCode: result.StatusCode);
                     }
 
                     Console.WriteLine($"Attempting next build number {nextBuildNumber}.");
@@ -170,46 +133,31 @@ public static class BuildTagNumber
     {
         GithubNewTagRef newTagRef = new($"refs/tags/{context.Prefix}build-number-{nextBuildNumber}", sha: context.Sha);
 
-        string content = JsonSerializer.Serialize(
-            value: newTagRef,
-            jsonTypeInfo: GithubApiJsonSerializerContext.Default.GithubNewTagRef
-        );
+        string content = JsonSerializer.Serialize(value: newTagRef, jsonTypeInfo: GithubApiJsonSerializerContext.Default.GithubNewTagRef);
+
         return new(content: content, encoding: Encoding.UTF8, new MediaTypeHeaderValue(MIME_TYPE));
     }
 
     [DoesNotReturn]
     private static int CouldNotUpdateBuildNumberRetriesExceeded(int nextBuildNumber, int maxAttempts)
     {
-        throw new BuildTagNumberException(
-            $"Failed to update build number {nextBuildNumber} - Too many attempts: {maxAttempts}"
-        );
+        throw new BuildTagNumberException($"Failed to update build number {nextBuildNumber} - Too many attempts: {maxAttempts}");
     }
 
     [DoesNotReturn]
     private static int CouldNotUpdateBuildNumberHttpError(int nextBuildNumber, HttpStatusCode statusCode)
     {
-        throw new BuildTagNumberException(
-            $"Failed to update build number {nextBuildNumber} - http error: {(int)statusCode}."
-        );
+        throw new BuildTagNumberException($"Failed to update build number {nextBuildNumber} - http error: {(int)statusCode}.");
     }
 
     private static HttpClient CreateClient(in GitHubContext context)
     {
-        HttpClient client = new()
-        {
-            BaseAddress = new("https://api.github.com/"),
-            DefaultRequestVersion = new(2, 0),
-            DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher,
-        };
+        HttpClient client = new() { BaseAddress = new("https://api.github.com/"), DefaultRequestVersion = new(major: 2, minor: 0), DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher };
         client.DefaultRequestHeaders.Authorization = new(scheme: "token", parameter: context.Token);
-        client.DefaultRequestHeaders.UserAgent.Add(
-            new(productName: VersionInformation.Product, productVersion: VersionInformation.Version)
-        );
+        client.DefaultRequestHeaders.UserAgent.Add(new(productName: VersionInformation.Product, productVersion: VersionInformation.Version));
         client.DefaultRequestHeaders.Accept.Add(new(MIME_TYPE));
         client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
 
         return client;
     }
-
-    private const string MIME_TYPE = "application/vnd.github+json";
 }
