936b7d9bea84ebae944019c6254146d0fedb95b2
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-07 14:54:42 +0000

    Cleanup: UriExtensions.net9.cs

--- src/FunFair.BuildVersion.GitTagBuildNumber/Extensions/UriExtensions.net9.cs
+++ src/FunFair.BuildVersion.GitTagBuildNumber/Extensions/UriExtensions.net9.cs
@@ -1,4 +1,4 @@
-﻿#if NET9_0_OR_GREATER
+#if NET9_0_OR_GREATER
 using System;
 using System.Buffers;
 
@@ -6,7 +6,10 @@ namespace FunFair.BuildVersion.GitTagBuildNumber.Extensions;
 
 public static partial class UriExtensions
 {
-    private static readonly SearchValues<string> HttpProtocols = SearchValues.Create([INSECURE, SECURE], comparisonType: StringComparison.Ordinal);
+    private static readonly SearchValues<string> HttpProtocols = SearchValues.Create(
+        [INSECURE, SECURE],
+        comparisonType: StringComparison.Ordinal
+    );
 
     public static bool IsHttp(this Uri uri)
     {
